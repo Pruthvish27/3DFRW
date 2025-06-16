7700858f177c56918bb7972f703caf752122d498
Dimey <73528592+Pruthvish27@users.noreply.github.com>
2025-06-16 20:02:23 +0530

    Added basic styling to the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import ProductList from './pages/ProductList';
 import ProductDetails from './pages/ProductDetails';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Logout from './components/Logout';
+import './App.css'; // Import the CSS file
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="container"> {/* Add a container div */}
         {/* Navigation (Header) - We'll create this later */}
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,6 +19,7 @@ function App() {
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
         {/* Footer - We'll create this later */}
       </div>
