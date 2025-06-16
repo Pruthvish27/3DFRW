f9a256d2b90599c2a24ef87f85651f7c68a40467
Dimey <73528592+Pruthvish27@users.noreply.github.com>
2025-06-16 19:59:02 +0530

    Added navigation links to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import authService from '../services/authService';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <Link to="/logout">Logout</Link>
-        </div>
-    );
+  const user = authService.getCurrentUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login'); // Redirect to home page if logged in
+    }
+  }, [navigate]);
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to our 3D Furniture Store!</p>
+      <ul>
+        <li>
+          <Link to="/products">View Products</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
